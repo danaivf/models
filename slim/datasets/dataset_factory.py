615c53f9e413f6e54471b998046a2afcacebba54
Ubuntu <ubuntu@ip-172-31-52-249.ec2.internal>
2017-02-13 18:06:07 +0000

    initial checkin

--- slim/datasets/dataset_factory.py
+++ slim/datasets/dataset_factory.py
@@ -22,12 +22,13 @@ from datasets import cifar10
 from datasets import flowers
 from datasets import imagenet
 from datasets import mnist
-
+from datasets import genea
 datasets_map = {
     'cifar10': cifar10,
     'flowers': flowers,
     'imagenet': imagenet,
     'mnist': mnist,
+    'genea':genea,
 }
 
 
